ee01bb9ef49a3d2156ccfa21322c2fe4c586f722
yuanjh6 <553882335@163.com>
2020-10-17 21:12:19 +0800

    tmp

--- source/conf.py
+++ source/conf.py
@@ -61,6 +61,7 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
